3009554bf4606aee518edfd9ce1550b130f6e6e7
Francisco Castillo <frank.gamedev@gmail.com>
2022-11-05 16:37:02 -0600

    BG - Menus

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+	
+	public void MainMenu()
+	{
+		SceneManager.LoadScene("StartScreen");
+	}
+	
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Level1");
+	}
+	
+	public void CreditsScene()
+	{
+		SceneManager.LoadScene("CreditsScene");
+	}
+	
+	public void ExitGame()
+	{
+		
+		if(Application.isEditor)
+		{
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
+		else
+		{
+			Application.Quit();
+		}
+	}
+}
