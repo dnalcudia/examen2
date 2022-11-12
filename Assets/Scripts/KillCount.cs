2d5511c5ecb55b537406a376bef52016021001cb
Francisco Castillo <frank.gamedev@gmail.com>
2022-11-12 10:28:21 -0600

    DoorLogic

--- Assets/Scripts/KillCount.cs
+++ Assets/Scripts/KillCount.cs
@@ -10,6 +10,7 @@ public class KillCount : MonoBehaviour
 	
 	public int KillCounter =0;
 	public TMP_Text TKC;
+	public GameObject Door1,Door2;
 	
 
 	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
@@ -22,6 +23,16 @@ public class KillCount : MonoBehaviour
     {
 	    Debug.Log(KillCounter);
 	    TKC.text = "Kill Count: " + KillCounter.ToString();
+	    
+	    if (KillCounter >= 10)
+	    {
+	    	Door1.SetActive(false);
+	    }
+	    
+	    if (KillCounter >= 25)
+	    {
+	    	Door2.SetActive(false);
+	    }
 	    
 	    if(KillCounter >= 50)
 	    {
